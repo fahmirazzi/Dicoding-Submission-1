c245cdf0a52317f3b378b1f1b90e45ed1762d023
Dzulfahmi <dzulfahmirazzie@gmail.com>
2024-03-06 14:55:20 +0700

    Maybe Last

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 let menu = document.querySelector("header .nav--list");
 let menuButton = [...document.querySelectorAll(".hamburger-menu")];
-// let cancelMenu = document.querySelector(".cancel--button");
-
-// console.log(typeof menuButton);
-// console.log(menuButton);
+let popUp = document.querySelector(".popup-signup_container");
+let bgPopup = document.querySelector(".bg-popup");
+let cancelPopup = document.querySelector(".cancel-popup");
+let PopUpOpened = false;
 
 menuButton.map((btn) => {
-  console.log(btn);
   btn.addEventListener("click", () => {
     menu.classList.toggle("hide--menu");
   });
 });
 
-// cancelMenu.addEventListener("click", () => {
-//   menu.classList.toggle("hide--menu");
-// });
+cancelPopup.addEventListener("click", () => {
+  popUp.classList.add("hide-popup");
+});
+
+bgPopup.addEventListener("click", () => {
+  popUp.classList.add("hide-popup");
+});
+
+window.addEventListener("scroll", () => {
+  let scrollTop = window.scrollY;
+  let docHeight = document.body.offsetHeight;
+  let winHeight = window.innerHeight;
+  let scrollPercent = scrollTop / (docHeight - winHeight);
+  let scrollPercentRounded = Math.round(scrollPercent * 100);
+
+  if (scrollPercentRounded > 50 && PopUpOpened == false) {
+    hidePopup();
+    PopUpOpened = true;
+  }
+  // percentLabel.innerHTML = scrollPercentRounded;
+  // document.title = `(${scrollPercentRounded}%) ${originalTitle}`;
+});
+
+function hidePopup() {
+  popUp.classList.remove("hide-popup");
+}
